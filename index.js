eccbcadf459fb4f9e0bd1778b494d229981bb3b3
Odima-dev <vincentodima22@gmail.com>
2025-03-19 14:38:24 +0300

    creating html elements in js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e2e16d960f40808e56f05efcae19e823&page=1'
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=e2e16d960f40808e56f05efcae19e823&query="
+
+const main = document.getElementById("section")
+const form = document.getElementById("form")
+const search = document.getElementById("search")
+
+function returnMovies(url) {
+    fetch(url).then(res => res.json())
+    .then(function(data) {
+        console.log(data.results)
+        data.results.forEach(element => {
+            const div_card = document.createElement('div')
+            const div_row = document.createElement('div')
+            const div_column = document.createElement('div')
+            const image = document.createElement('img')
+            const title = document.createElement('h3')
+        })
+    })
+}
