0f67e443f44f8ac67ae9178251a64192d4832b44
Odima-dev <vincentodima22@gmail.com>
2025-03-19 14:16:19 +0300

    defining API constants for moviedb

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const APILINK = ''
-const IMG_PATH = ''
-
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e2e16d960f40808e56f05efcae19e823&page=1'
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=e2e16d960f40808e56f05efcae19e823&query="
