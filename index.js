451bccbbbbb19fc5093cbae89089531873936cdf
Odima-dev <vincentodima22@gmail.com>
2025-03-21 14:00:47 +0300

    setting up mongodb

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import ReviewsDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
-const mongo_password = process.env['MONGO_PASSWORD']\
-const url = `mongodb
-mongodb+srv://${mongo_username}:${mongo_password}@cluster0.440or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.440or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const port = 8000
 
@@ -16,5 +15,13 @@ MongoClient.connect(
         maxPoolSize: 50,
         wtimeoutMS: 2500,
         useNewUrlParser: true
-    }
-)
+    })
+.catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    })
+})
