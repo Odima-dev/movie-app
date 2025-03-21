20e50f9c1e8ecc2589041caa404ee5b839819847
Odima-dev <vincentodima22@gmail.com>
2025-03-21 16:11:11 +0300

    creating reviewscontroller file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-//import ReviewsDAO from "./dao/reviewsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 import dotenv from "dotenv"
 dotenv.config()
 
@@ -23,6 +23,7 @@ MongoClient.connect(
     process.exit(1)
 })
 .then(async client => {
+    await ReviewsDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listening on port ${port}`)
     })
