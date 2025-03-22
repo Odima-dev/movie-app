ad3329dce98c0d43ec65eaf693b2f100bba7946e
Odima-dev <vincentodima22@gmail.com>
2025-03-22 03:17:33 +0300

    linking front end to backend

--- movie.js
+++ movie.js
@@ -1,6 +1,4 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e2e16d960f40808e56f05efcae19e823&page=1'
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=e2e16d960f40808e56f05efcae19e823&query="
+const APILINK = 'http://127.0.0.1:8000/api/v1/reviews/'
 
 const main = document.getElementById("section")
 const form = document.getElementById("form")
