60d746f68a49c1d5a788692eebd43ffcb8b7a72c
Odima-dev <vincentodima22@gmail.com>
2025-03-22 05:26:02 +0300

    linking front end to backend

--- movie.js
+++ movie.js
@@ -19,26 +19,6 @@ function returnReviews(url) {
             div_card.setAttribute('class', 'card')
 
             const div_row = document.createElement('div')
-            div_row.setAttribute('class', 'row')
-
-            const div_column = document.createElement('div')
-            div_column.setAttribute('class', 'column')
-
-            const image = document.createElement('img')
-            image.setAttribute('class', 'thumbnail')
-            image.setAttribute('id', 'image')
-
-            const title = document.createElement('h3')
-            title.setAttribute('id', 'title')
-
-            const center = document.createElement('center')
-
-            title.innerHTML = `${element.title}`
-            image.src = IMG_PATH + element.poster_path
-
-            center.appendChild(image)
-            div_card.appendChild(center)
-            div_card.appendChild(title)
             div_column.appendChild(div_card)
             div_row.appendChild(div_column)
 
